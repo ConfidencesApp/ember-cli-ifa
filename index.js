94c7d52eafa3f554d614bda4c7d1b14021c983ad
BnitoBzh <fievet.benoit@gmail.com>
2019-06-03 17:46:49 +0200

    use empty assetMap by default if ifa is disabled
    prevents get-asset-map-data util error

--- index.js
+++ index.js
@@ -37,43 +37,43 @@ module.exports = {
     const env = process.env.EMBER_ENV;
     const ifaConfig = this.project.config(env).ifa;
 
-    if (!ifaConfig.enabled) {
-      return;
-    }
+    let assetMap = null;
+    if (ifaConfig.enabled) {
 
-    let fingerprintPrepend = '/';
+      let fingerprintPrepend = '/';
 
-    const files = fs.readdirSync(path.join(build.directory, 'assets'));
-    const totalFiles = files.length;
+      const files = fs.readdirSync(path.join(build.directory, 'assets'));
+      const totalFiles = files.length;
 
-    let assetFileName = null;
-    for (let i = 0; i < totalFiles; i++) {
-      if (files[i].match(/^assetMap/i)) {
-        assetFileName = files[i];
-        break;
+      let assetFileName = null;
+      for (let i = 0; i < totalFiles; i++) {
+        if (files[i].match(/^assetMap/i)) {
+          assetFileName = files[i];
+          break;
+        }
       }
-    }
 
-    // Prepend the URL of the assetMap with the location defined in fingerprint
-    // options.
-    if (this.app && this.app.options && this.app.options.fingerprint) {
-      fingerprintPrepend = this.app.options.fingerprint.prepend;
-    }
+      // Prepend the URL of the assetMap with the location defined in fingerprint
+      // options.
+      if (this.app && this.app.options && this.app.options.fingerprint) {
+        fingerprintPrepend = this.app.options.fingerprint.prepend;
+      }
 
-    const assetFileNamePath = `${build.directory}/assets/${assetFileName}`;
+      const assetFileNamePath = `${build.directory}/assets/${assetFileName}`;
 
-    // When using fastboot, always use the inline form
-    // As ajax is not so easily possible there
-    if (!ifaConfig.inline && this._isFastBoot) {
-      this.ui.writeLine('When running fastboot, ember-cli-ifa is forced into inline mode.');
-    }
-    const inline = ifaConfig.inline || this._isFastBoot;
+      // When using fastboot, always use the inline form
+      // As ajax is not so easily possible there
+      if (!ifaConfig.inline && this._isFastBoot) {
+        this.ui.writeLine('When running fastboot, ember-cli-ifa is forced into inline mode.');
+      }
+      const inline = ifaConfig.inline || this._isFastBoot;
 
-    let assetMap;
-    if (inline && fs.existsSync(assetFileNamePath)) {
-      assetMap = JSON.parse(fs.readFileSync(assetFileNamePath, { encoding: 'utf-8' }));
-    } else if (assetFileName) {
-      assetMap = `${fingerprintPrepend}assets/${assetFileName}`;
+      let assetMap;
+      if (inline && fs.existsSync(assetFileNamePath)) {
+        assetMap = JSON.parse(fs.readFileSync(assetFileNamePath, { encoding: 'utf-8' }));
+      } else if (assetFileName) {
+        assetMap = `${fingerprintPrepend}assets/${assetFileName}`;
+      }
     }
 
     replacePlaceholder(path.join(build.directory, 'index.html'), assetMap);
